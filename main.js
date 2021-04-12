8fd95a524bfd8e7aae6c26ac8a35ee5728bd5fc2
Ashton Huxtable <aehuxtable@gmail.com>
2021-04-11 18:43:49 -0600

    Add function to clear recipe and to disable lets cook

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var mainRadioBtn = document.querySelector('#rd-main')
 var dessertRadioBtn = document.querySelector('#rd-dessert')
 var recipeFormBtn = document.querySelector('#add-recipe')
 var addNewBtn = document.querySelector('#add-new')
+var clearBtn = document.querySelector('#clear-recipe')
 
 var cookPot = document.querySelector('img')
 var footer = document.querySelector('footer')
@@ -18,13 +19,21 @@ showRecipeBtn.addEventListener('click', displayRecipe)
 recipeFormBtn.addEventListener('click', displayAddForm)
 addNewBtn.addEventListener('click', addRecipeToList)
 addNewBtn.addEventListener('click', displayNewRecipe)
+clearBtn.addEventListener('click', clearRecipe)
+mainRadioBtn.addEventListener('click', enableBtn)
+sidesRadioBtn.addEventListener('click', enableBtn)
+dessertRadioBtn.addEventListener('click', enableBtn)
 
+function enableBtn() {
+  showRecipeBtn.classList.add('fadeIn')
+}
 
 function displayRecipe() {
   var randomSide = sides[getRandomIndex(sides)];
   var randomMain = mains[getRandomIndex(mains)];
   var randomDessert = desserts[getRandomIndex(desserts)];
-  cookPot.classList.add('hidden')
+  cookPot.classList.toggle('hidden')
+  clearBtn.classList.toggle('fadeIn')
   if (sidesRadioBtn.checked) {
     displayedRecipe.innerHTML = `
       <h5 class='recipe'>You should make:</h5>
@@ -39,6 +48,7 @@ function displayRecipe() {
           <h3 class='main'>${randomDessert}</h3>`
     }
   }
+
 
 function displayAddForm() {
   footer.classList.toggle('fadeIn');
@@ -61,10 +71,19 @@ function displayNewRecipe () {
     addNewBtn.disabled = true;
   } else {
     cookPot.classList.add('hidden');
+    clearBtn.classList.toggle('fadeIn');
     displayedRecipe.innerHTML = `
     <h5 class='recipe'>You should make:</h5>
     <h3 class='main'>${recipeName.value}</h3>`
   }
+  displayAddForm();
+  addNewBtn.disabled = false;
+}
+
+function clearRecipe() {
+  displayedRecipe.classList.toggle('hidden');
+  cookPot.classList.remove('hidden');
+  clearBtn.classList.remove('fadeIn')
 }
 
 function getRandomIndex(array) {
